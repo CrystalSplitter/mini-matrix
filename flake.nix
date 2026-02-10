e74c72fee8ae799491c1e9641817c50cf0537369
CrystalSplitter <crystal@crystalwobsite.gay>
2026-02-09 17:59:15 -0800

    Add initial flake.nix

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }: 
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    packages.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.default = self.packages.x86_64-linux.hello;
+    formatter = pkgs.nixfmt-tree;
+  });
+}
