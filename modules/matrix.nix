d9683f1eafb0ddf4b629093cc63aa98526257d62
CrystalSplitter <crystal@crystalwobsite.gay>
2026-02-17 17:53:35 -0800

    Remove dead token

--- modules/matrix.nix
+++ modules/matrix.nix
@@ -14,7 +14,8 @@ in
         allow_registration = true;
         allow_encryption = false;
         allow_federation = true;
-        registration_token = "1722 e6f1 150c be1f 4bce ff00 007c 32eb";
+        # Generate this with 'pwgen -s 64 1' maybe?
+        registration_token = "";
         url_preview_url_contains_allowlist = [ "*" ];
 
         # TURN config.
